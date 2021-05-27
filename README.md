c5cbbe6cdc3eafa453c650bf0004621830bd39df
Henrique Beier <ahoy@harbortype.com>
2021-05-27 10:10:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Show TrueType Curves
+
+This is a plugin for the [Glyphs font editor](https://glyphsapp.com/).  
+
+This reporter plugin draws the contents of the active layer as quadratic curves in the background. This will give you an idea of how the points will be when exporting as TTF.
+
+<img src="images/showTrueTypeCurves.png" width="336" height="400">
+
+Keep in mind this is not the same conversion Glyphs does when exporting the fonts, so the results may vary a bit.
+
+### License
+
+Copyright 2021 [Henrique Beier](https://www.harbortype.com) @harbortype
+
+Made possible with the [GlyphsSDK](https://github.com/schriftgestalt/GlyphsSDK) by Georg Seifert (@schriftgestalt), Jan Gerner (@yanone) and Rainer Erich Scheichelbauer (@mekkablue). 
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
---
+++ Show TrueType Curves.glyphsReporter/Contents/Resources/plugin.py
@@ -0,0 +1,129 @@
+# encoding: utf-8
+from __future__ import division, print_function, unicode_literals
+import objc, math, traceback
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+###########################################################################################################
+#
+#
+#	Reporter Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Reporter
+#
+#
+###########################################################################################################
+
+
+
+
+class showTrueTypeCurves(ReporterPlugin):
+
+	@objc.python_method 
+	def settings(self):
+		self.menuName = Glyphs.localize({
+			'en': u'TrueType Curves',
+			'pt': u'Curvas TrueType',
+
+		})
+		self.thisMenuTitle = {"name": u"%s:" % self.menuName, "action": None }
+		self.masterIds = []
+
+
+	@objc.python_method 
+	def refreshView(self):
+		try:
+			Glyphs = NSApplication.sharedApplication()
+			currentTabView = Glyphs.font.currentTab
+			if currentTabView:
+				currentTabView.graphicView().setNeedsDisplay_(True)
+		except Exception as e:
+			pass
+
+
+	@objc.python_method 
+	def getHandleSize(self):
+		""" Get the handle size in scale """
+		handleSizes = (5, 8, 12)
+		handleSizeIndex = Glyphs.handleSize 
+		handleSize = handleSizes[handleSizeIndex] * self.getScale() ** 0.1 # scaled diameter
+		return handleSize
+
+
+	@objc.python_method 
+	def getMasterIDs(self, layer):
+		""" Get the masters and special layers IDs """
+		masterIds = set()
+		glyph = layer.parent
+		for lyr in glyph.layers:
+			if lyr.isSpecialLayer or lyr.layerId == lyr.associatedMasterId:
+				masterIds.add(lyr.layerId)
+		return list(masterIds)
+
+
+	@objc.python_method 
+	def drawTrueTypeCurves(self, layer, path, scale):
+		radius = 2.5
+		currentTab = Glyphs.font.currentTab
+		origin = currentTab.selectedLayerOrigin
+
+		# Get the nodes
+		for node in path.nodes:
+			nextNode = node.nextNode
+			x = node.position.x
+			y = node.position.y
+			dx = nextNode.position.x - x
+			dy = nextNode.position.y - y
+			offcurvePosition = NSPoint( (x+dx) * scale + origin[0], (y+dy) * scale + origin[1] )
+
+			# Draw line
+			# NSColor.colorWithCalibratedRed_green_blue_alpha_(1, .8, .75, 1).set()
+			NSColor.colorWithCalibratedRed_green_blue_alpha_(.8, .8, .8, .8).set()
+			line = NSBezierPath.bezierPath()
+			line.setLineWidth_(1)
+			line.moveToPoint_( NSPoint( (node.position.x) * scale + origin[0], (node.position.y) * scale + origin[1] ) )
+			line.lineToPoint_(offcurvePosition)
+			line.stroke()
+			
+			if node.type != OFFCURVE:
+				continue
+
+			# Draw nodes
+			circle = NSRect()
+			circle.size = NSSize(radius * 2 , radius * 2)
+			circle.origin = NSPoint( (node.position.x) * scale + origin[0] - radius, (node.position.y) * scale + origin[1] - radius )
+			NSBezierPath.bezierPathWithRoundedRect_xRadius_yRadius_(circle, radius, radius).fill()
+		
+
+	@objc.python_method 
+	def foregroundInViewCoords(self, layer=None): 
+		""" Draw stuff on the screen """
+		pass
+
+	@objc.python_method 
+	def backgroundInViewCoords(self, layer=None):
+		""" Draw stuff in the background """
+		layer = Glyphs.font.selectedLayers[0]
+		self.masterIds = self.getMasterIDs(layer)
+		scale = self.getScale()
+		handleSize = self.getHandleSize()
+		glyph = layer.parent
+		# if len(self.masterIds) <= 1:
+		# 	return
+		if layer.layerId not in self.masterIds:
+			return
+		# if len(Glyphs.font.masters) > 1 and not glyph.mastersCompatible:
+		# 	return
+		if not layer.paths:
+			return
+		
+		for p, path in enumerate(layer.paths):
+			dummyPath = path.copy()
+			dummyPath.convertToQuadratic()
+			self.drawTrueTypeCurves(layer, dummyPath, scale)
+
+
+	@objc.python_method 
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
